f3080b7ddb5b4fa8ce33fe4dcfd39715d0c29a4d
zunza <fksfks015@naver.com>
2024-03-22 22:12:03 +0900

    Feat: Add ProductImage class

--- src/main/java/zunza/myshop/domain/ProductImage.java
+++ src/main/java/zunza/myshop/domain/ProductImage.java
@@ -1,2 +1,48 @@
-package zunza.myshop.domain;public class ProductImage {
+package zunza.myshop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "PRODUCT_IMAGES")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "PRODUCT_IMAGE_ID")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "PRODUCT_ID")
+	private Product product;
+
+	@Column(name = "PRODUCT_IMAGE_NAME")
+	private String imageName;
+
+	@Column(name = "PRODUCT_IMAGE_URL")
+	private String imageUrl;
+
+	@Builder
+	public ProductImage(
+		Product product,
+		String imageName,
+		String imageUrl) {
+
+		this.product = product;
+		this.imageName = imageName;
+		this.imageUrl = imageUrl;
+	}
 }
