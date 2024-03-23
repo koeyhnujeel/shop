50c0122bf2fea7f2f57a6327b7216bc26a98039c
zunza <fksfks015@naver.com>
2024-03-23 16:54:50 +0900

    Feat: Add ProductOption Request 객체 상품 옵션 정보를 가진 요청 객체 구현

---
+++ src/main/java/zunza/myshop/request/ProductOptionAddRequest.java
@@ -0,0 +1,29 @@
+package zunza.myshop.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import zunza.myshop.domain.ProductOption;
+
+@Getter
+@NoArgsConstructor
+public class ProductOptionAddRequest {
+
+	@NotBlank(message = "사이즈를 선택해주세요.")
+	private String size;
+
+	@NotBlank(message = "색상을 입력해주세요.")
+	private String color;
+
+	@NotNull(message = "재고량을 입력해주세요.")
+	private Integer stock;
+
+	public ProductOption toEntity() {
+		return ProductOption.builder()
+			.size(this.size)
+			.color(this.color)
+			.stock(this.stock)
+			.build();
+	}
+}
