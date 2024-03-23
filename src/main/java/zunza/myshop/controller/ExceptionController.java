3f05093d9736010e9117f72655e51e293f361dc2
zunza <fksfks015@naver.com>
2024-03-23 23:52:51 +0900

    Feat: Add ExceptionController 예외 처리를 담당하는 컨트롤러 구현

---
+++ src/main/java/zunza/myshop/controller/ExceptionController.java
@@ -0,0 +1,46 @@
+package zunza.myshop.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+
+import zunza.myshop.response.ErrorResponse;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ErrorResponse invalidRequest(MethodArgumentNotValidException e) {
+		ErrorResponse response = ErrorResponse.builder()
+			.message("잘못된 요청입니다.")
+			.code("400")
+			.build();
+
+		for (FieldError fieldError : e.getFieldErrors()) {
+			response.addError(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		return response;
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MissingServletRequestPartException.class)
+	public ErrorResponse missingRequestPart(MissingServletRequestPartException e) {
+		ErrorResponse errorResponse = ErrorResponse.builder()
+			.message("잘못된 요청입니다.")
+			.code("400")
+			.build();
+
+		if (e.getRequestPartName().equals("mainImage")) {
+			errorResponse.addError("mainImage", "대표 이미지를 업로드 해주세요.");
+		} else if (e.getRequestPartName().equals("images")) {
+			errorResponse.addError("images", "서브 이미지를 업로드 해주세요.");
+		}
+
+		return errorResponse;
+	}
+}
