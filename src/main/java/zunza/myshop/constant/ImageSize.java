90cb707b0c162c9def4cedc0fccb4c776fa9b10a
zunza <fksfks015@naver.com>
2024-03-23 16:47:32 +0900

    Feat: Add IamgeSize constant

---
+++ src/main/java/zunza/myshop/constant/ImageSize.java
@@ -0,0 +1,9 @@
+package zunza.myshop.constant;
+
+public class ImageSize {
+	public static final int NORMAL_IMAGE_WIDTH = 600;
+	public static final int NORMAL_IMAGE_HEIGHT = 700;
+
+	public static final int THUMBNAIL_IMAGE_WIDTH = 175;
+	public static final int THUMBNAIL_IMAGE_HEIGHT = 200;
+}
