92137e5459ed0a1a46fe8ad506897ae153e86f40
zunza <fksfks015@naver.com>
2024-03-25 21:13:55 +0900

    Feat: Add 상품 리스트 조회 서비스 메서드

--- src/main/java/zunza/myshop/service/ProductService.java
+++ src/main/java/zunza/myshop/service/ProductService.java
@@ -1,7 +1,9 @@
 package zunza.myshop.service;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,8 +15,10 @@ import zunza.myshop.domain.ProductOption;
 import zunza.myshop.repository.ProductImageRepository;
 import zunza.myshop.repository.ProductOptionRepository;
 import zunza.myshop.repository.ProductRepository;
-import zunza.myshop.request.ProductAddRequest;
-import zunza.myshop.request.ProductOptionAddRequest;
+import zunza.myshop.request.ProductRequest;
+import zunza.myshop.request.ProductOptionRequest;
+import zunza.myshop.response.LatestProductResponse;
+import zunza.myshop.response.TopSalesProductResponse;
 import zunza.myshop.util.ImageUtil;
 
 @Service
@@ -28,20 +32,56 @@ public class ProductService {
 
 
 	public void addProduct(
-		ProductAddRequest productAddRequest,
-		ProductOptionAddRequest productOptionAddRequest,
+		ProductRequest productRequest,
+		ProductOptionRequest productOptionARequest,
 		MultipartFile mainImage,
 		List<MultipartFile> images
 		) throws IOException {
 
-		Product product = productAddRequest.toEntity();
+		Product product = Product.from(productRequest);
 		productRepository.save(product);
 
-		ProductOption productOption = productOptionAddRequest.toEntity();
+		ProductOption productOption = ProductOption.from(productOptionARequest);
 		productOption.setRelation(product);
 		productOptionRepository.save(productOption);
 
 		List<ProductImage> productImages = imageUtil.convertToEntitiesWithResizeAndSave(mainImage, images);
 		productImages.forEach(productImage -> productImageRepository.save(productImage.setRelation(product)));
+	}
+
+	public Map<String, Object> findProductList() {
+
+		List<Product> topSalesProductsWithImages = productRepository.findMainViewProductsWithImage("sales");
+		List<TopSalesProductResponse> topSalesProducts = topSalesProductsWithImages.stream()
+			.map(product -> TopSalesProductResponse.of(
+				product.getId(),
+				product.getProductName(),
+				product.getPrice(),
+				getThumbnailUrl(product)))
+			.toList();
+
+		List<Product> latestProductsWithImages = productRepository.findMainViewProductsWithImage("latest");
+		List<LatestProductResponse> latestProducts = latestProductsWithImages.stream()
+			.map(product -> LatestProductResponse.of(
+				product.getId(),
+				product.getProductName(),
+				product.getPrice(),
+				getThumbnailUrl(product)))
+			.toList();
+
+		Map<String, Object> mainViewResponse = new HashMap<>();
+		mainViewResponse.put("topSalesProducts", topSalesProducts);
+		mainViewResponse.put("latestProducts", latestProducts);
+
+		return mainViewResponse;
+	}
+
+	private String getThumbnailUrl(Product product) {
+		return String.valueOf(product.getImages().stream()
+			.filter(productImage -> productImage.getImageName().startsWith("thumbnail"))
+			.findFirst()
+			.map(ProductImage::getImageUrl)
+			.orElse("")
+		);
 	}
 }
