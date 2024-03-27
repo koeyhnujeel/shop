de5ae1457d150dbc670bbe30ce0ecfbc8054ac25
zunza <fksfks015@naver.com>
2024-03-27 23:55:16 +0900

    Feat: 상품 상세 API 추가

--- src/main/java/zunza/myshop/controller/ProductController.java
+++ src/main/java/zunza/myshop/controller/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,6 +17,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import zunza.myshop.request.ProductRequest;
 import zunza.myshop.request.ProductOptionRequest;
+import zunza.myshop.response.ProductDetails;
 import zunza.myshop.service.ProductService;
 
 @RestController
@@ -28,17 +30,23 @@ public class ProductController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public void productAdd(
 		@RequestPart("productRequest") @Valid ProductRequest productRequest,
-		@RequestPart("productOptionRequest") @Valid ProductOptionRequest productOptionRequest,
+		@RequestPart("productOptionsRequest") @Valid List<ProductOptionRequest> productOptionsRequest,
 		@RequestPart("mainImage") MultipartFile mainImage,
 		@RequestPart("images") List<MultipartFile> images
 		) throws IOException {
 
-		productService.addProduct(productRequest, productOptionRequest, mainImage, images);
+		productService.addProduct(productRequest, productOptionsRequest, mainImage, images);
 	}
 
 	@GetMapping("/products")
 	@ResponseStatus(HttpStatus.OK)
 	public Map<String, Object> productList() {
 		return productService.findProductList();
+	}
+
+	@GetMapping("/products/{productId}")
+	@ResponseStatus(HttpStatus.OK)
+	public ProductDetails productDetails(@PathVariable("productId") Long productId) {
+		return productService.findProduct(productId);
 	}
 }
