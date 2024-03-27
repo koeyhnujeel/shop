d762f6a517d486fd107550f6a57e3e44f06e5194
zunza <fksfks015@naver.com>
2024-03-27 23:53:14 +0900

    Feat: 상품 리뷰를 조회하는 메서드 추가 유저 nickname까지 한번에 가져오기 위해 join fetch 사용

--- src/main/java/zunza/myshop/repository/ProductReviewRepository.java
+++ src/main/java/zunza/myshop/repository/ProductReviewRepository.java
@@ -1,8 +1,17 @@
 package zunza.myshop.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import zunza.myshop.domain.ProductReview;
 
+@Repository
 public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
+
+	@Query("select DISTINCT r from ProductReview r join fetch r.user where r.product.id = :ProductId")
+	List<ProductReview> findReviewsAndUserByProductId(@Param("ProductId") Long ProductId);
 }
