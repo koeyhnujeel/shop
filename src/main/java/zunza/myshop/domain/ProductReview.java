87f657ec66dc0afbb4b7d24ccb0b90af40c414a6
zunza <fksfks015@naver.com>
2024-03-27 15:41:19 +0900

    Feat: ProductReview 클래스 추가 ProductReview, User -> N : 1 단방향 매핑 ProductReview, Product -> N : 1 단방향 매핑

---
+++ src/main/java/zunza/myshop/domain/ProductReview.java
@@ -0,0 +1,70 @@
+package zunza.myshop.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "PRODUCT_REVIEW")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductReview {
+
+	@Id
+	@Column(name = "PRODUCT_REVIEW_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "USER_ID")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "PRODUCT_ID")
+	private Product product;
+
+	@Lob
+	@Column(name = "PRODUCT_REVIEW_CONTENT", nullable = false)
+	private String content;
+
+	@Column(name = "PRODUCT_REVIEW_CREATED_DATE", nullable = false)
+	private LocalDateTime createdAt;
+
+	@Builder
+	private ProductReview(
+		User user,
+		Product product,
+		String content,
+		LocalDateTime createdAt) {
+
+		this.user = user;
+		this.product = product;
+		this.content = content;
+		this.createdAt = LocalDateTime.now().withNano(0);
+	}
+
+	public static ProductReview of(
+		User user,
+		Product product,
+		ReviewRequest reviewRequest) {
+
+		return ProductReview.builder()
+			.user(user)
+			.product(product)
+			.content(reviewRequest.content)
+			.build();
+	}
+}
