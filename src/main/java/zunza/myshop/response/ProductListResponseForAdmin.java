87ece22a3d56621dd5cf610c98dbc271be0a0bcf
zunza <fksfks015@naver.com>
2024-03-29 19:50:11 +0900

    Feat: 상품 관리 페이지용 응답 클래스 추가

--- src/main/java/zunza/myshop/response/ProductListResponseForAdmin.java
+++ src/main/java/zunza/myshop/response/ProductListResponseForAdmin.java
@@ -1,2 +1,34 @@
-package zunza.myshop.response;public class ProductListResponseForAdmin {
+package zunza.myshop.response;
+
+import lombok.Getter;
+import zunza.myshop.domain.Product;
+import zunza.myshop.domain.ProductImage;
+
+@Getter
+public class ProductListResponseForAdmin {
+
+	private Long id;
+	private String productName;
+	private String thumbnailUrl;
+
+	private ProductListResponseForAdmin(
+		Long id,
+		String productName,
+		String thumbnailUrl) {
+
+		this.id = id;
+		this.productName = productName;
+		this.thumbnailUrl = thumbnailUrl;
+	}
+
+	public static ProductListResponseForAdmin from(Product product) {
+		return new ProductListResponseForAdmin(
+			product.getId(),
+			product.getProductName(),
+			product.getImages().stream()
+				.filter(productImage -> productImage.getImageName().startsWith("thumbnail"))
+				.findFirst()
+				.map(ProductImage::getImageUrl)
+				.orElse(""));
+	}
 }
