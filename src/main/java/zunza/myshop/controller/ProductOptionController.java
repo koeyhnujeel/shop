868b717936cd2d1988377ba073c6dc91e4827011
zunza <fksfks015@naver.com>
2024-03-29 23:45:50 +0900

    Feat: 상품 옵션 수정 API 구현

--- src/main/java/zunza/myshop/controller/ProductOptionController.java
+++ src/main/java/zunza/myshop/controller/ProductOptionController.java
@@ -3,6 +3,7 @@ package zunza.myshop.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import zunza.myshop.request.ProductOptionRequest;
+import zunza.myshop.request.ProductOptionUpdateRequest;
 import zunza.myshop.service.ProductOptionService;
 
 @RestController
@@ -26,5 +28,14 @@ public class ProductOptionController {
 		@RequestBody List<ProductOptionRequest> productOptionRequests) {
 
 		productOptionService.addProductOptions(productId, productOptionRequests);
+	}
+
+	@PatchMapping("/product-options/{product-optionId}")
+	@ResponseStatus(HttpStatus.OK)
+	public void productOptionModify(
+		@PathVariable("product-optionId") Long productOptionId,
+		@RequestBody ProductOptionUpdateRequest productOptionUpdateRequest) {
+
+		productOptionService.modifyProductOption(productOptionId, productOptionUpdateRequest);
 	}
 }
--- src/main/java/zunza/myshop/service/ProductOptionService.java
+++ src/main/java/zunza/myshop/service/ProductOptionService.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import zunza.myshop.domain.Product;
 import zunza.myshop.domain.ProductOption;
 import zunza.myshop.exception.ProductNotFoundException;
+import zunza.myshop.exception.ProductOptionNotFoundException;
 import zunza.myshop.repository.ProductOptionRepository;
 import zunza.myshop.repository.ProductRepository;
 import zunza.myshop.request.ProductOptionRequest;
+import zunza.myshop.request.ProductOptionUpdateRequest;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +32,16 @@ public class ProductOptionService {
 		productOptionRequests.forEach(productOptionRequest -> productOptionRepository.save(
 				ProductOption.from(productOptionRequest).setRelation(product)
 			));
+	}
+
+	@Transactional
+	public void modifyProductOption(
+		Long productOptionId,
+		ProductOptionUpdateRequest productOptionUpdateRequest) {
+
+		ProductOption productOption = productOptionRepository.findById(productOptionId)
+			.orElseThrow(() -> new ProductOptionNotFoundException(productOptionId));
+
+		productOption.update(productOptionUpdateRequest);
 	}
 }
