715d412b7ded6f8245fcc227f7d51ef931167fe3
zunza <fksfks015@naver.com>
2024-03-29 21:17:21 +0900

    Feat: findProductAndImageFetchJoin 메서드 추가 상대적으로 데이터가 많은 상품 이미지와 join fetch하여 상품 조회

--- src/main/java/zunza/myshop/repository/ProductRepository.java
+++ src/main/java/zunza/myshop/repository/ProductRepository.java
@@ -1,10 +1,20 @@
 package zunza.myshop.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import zunza.myshop.domain.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+
+	@Query("SELECT p "
+		+ "FROM Product p "
+		+ "JOIN FETCH p.images "
+		+ "WHERE p.id = :productId")
+	Optional<Product> findProductAndImageFetchJoin(@Param("productId") Long productId);
 }
