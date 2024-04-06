bf1004f718c8a1da037f28b25ccdc3207656cf39
zunza <fksfks015@naver.com>
2024-04-06 13:36:57 +0900

    Feat: createImage, updateImage, updateMainImage 메서드 추가

--- src/main/java/zunza/myshop/util/ImageUtil.java
+++ src/main/java/zunza/myshop/util/ImageUtil.java
@@ -2,6 +2,7 @@ package zunza.myshop.util;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,6 +23,7 @@ import zunza.myshop.domain.ProductImage;
 public class ImageUtil {
 
 	public static final String PATH = System.getProperty("user.dir") + "/src/main/resources/static/images/";
+	public static final String RESPONSE_PATH = "http://localhost:8080/";
 
 	private byte[] resizeImage(MultipartFile file, int width, int height) throws IOException {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -36,7 +38,39 @@ public class ImageUtil {
 		String extension = fileName.split("\\.")[1];
 		Path path = Paths.get(PATH + type + uuid + "." + extension);
 		Files.write(path, imageBytes);
-		return PATH + type + uuid + "." + extension;
+		return RESPONSE_PATH + type + uuid + "." + extension;
+	}
+
+	private ProductImage createImage(
+		MultipartFile productImage,
+		int width,
+		int height,
+		String type) throws IOException {
+
+		byte[] resizeImage = resizeImage(productImage, width, height);
+		String imageUrl = saveImage(resizeImage, type, productImage.getOriginalFilename());
+		String imageName = imageUrl.split(RESPONSE_PATH)[1];
+
+		return ProductImage.of(imageName, imageUrl);
+	}
+
+	private void updateImage(
+		MultipartFile newImage,
+		ProductImage productImage,
+		int width,
+		int height,
+		String type) throws IOException {
+
+		byte[] resizeImage = resizeImage(newImage, width, height);
+		String imageUrl = saveImage(resizeImage, type, newImage.getOriginalFilename());
+		String imageName = imageUrl.split(RESPONSE_PATH)[1];
+
+		productImage.update(imageName, imageUrl);
+	}
+
+	private void removeImage(String imageUrl) {
+		File file = new File(imageUrl);
+		file.delete();
 	}
 
 	public List<ProductImage> convertToEntitiesWithResizeAndSave (
@@ -45,27 +79,40 @@ public class ImageUtil {
 
 		List<ProductImage> productImages = new ArrayList<>();
 
-
 		// 섬네일 이미지 처리
-		byte[] thumbnailImage = resizeImage(mainImage, ImageSize.THUMBNAIL_IMAGE_WIDTH, ImageSize.THUMBNAIL_IMAGE_HEIGHT);
-		String thumbnailUrl = saveImage(thumbnailImage, "thumbnail-", mainImage.getOriginalFilename());
-		String thumbnailImageName = thumbnailUrl.split(PATH)[1];
-		productImages.add(ProductImage.of(thumbnailImageName, thumbnailUrl));
+		ProductImage thumbnailImage = createImage(
+			mainImage,
+			ImageSize.THUMBNAIL_IMAGE_WIDTH,
+			ImageSize.THUMBNAIL_IMAGE_HEIGHT,
+			"thumbnail-");
+		productImages.add(thumbnailImage);
 
 		// 메인 이미지 처리
-		byte[] resizedMainImage = resizeImage(mainImage, ImageSize.NORMAL_IMAGE_WIDTH, ImageSize.NORMAL_IMAGE_HEIGHT);
-		String mainImageUrl = saveImage(resizedMainImage, "main-", mainImage.getOriginalFilename());
-		String mainImageName = mainImageUrl.split(PATH)[1];
-		productImages.add(ProductImage.of(mainImageName, mainImageUrl));
+		ProductImage main = createImage(
+			mainImage,
+			ImageSize.NORMAL_IMAGE_WIDTH,
+			ImageSize.NORMAL_IMAGE_HEIGHT,
+			"main-");
+		productImages.add(main);
 
 		// 추가 이미지 처리
 		for (MultipartFile image : images) {
-			byte[] resizedImage = resizeImage(image, ImageSize.NORMAL_IMAGE_WIDTH, ImageSize.NORMAL_IMAGE_HEIGHT);
-			String subImageUrl = saveImage(resizedImage, "sub-", image.getOriginalFilename());
-			String subImageName = subImageUrl.split(PATH)[1];
-			productImages.add(ProductImage.of(subImageName, subImageUrl));
+			ProductImage subImage = createImage(
+				image,
+				ImageSize.NORMAL_IMAGE_WIDTH,
+				ImageSize.NORMAL_IMAGE_HEIGHT,
+				"sub-");
+			productImages.add(subImage);
 		}
 
 		return productImages;
+	}
+
+	public void updateMainImage(MultipartFile newImage, ProductImage mainImage, ProductImage thumbnail) throws IOException {
+		removeImage(mainImage.getImageUrl());
+		removeImage(thumbnail.getImageUrl());
+
+		updateImage(newImage, mainImage, ImageSize.NORMAL_IMAGE_WIDTH, ImageSize.NORMAL_IMAGE_HEIGHT, "main-");
+		updateImage(newImage, thumbnail, ImageSize.THUMBNAIL_IMAGE_WIDTH, ImageSize.THUMBNAIL_IMAGE_HEIGHT, "thumbnail");
 	}
 }
