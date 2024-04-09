defb3b2fb10868b6972d53840d83868756f3f2b7
zunza <fksfks015@naver.com>
2024-04-09 19:58:15 +0900

    workflow test commit

--- src/test/java/zunza/myshop/MyShopApplicationTests.java
+++ src/test/java/zunza/myshop/MyShopApplicationTests.java
@@ -2,8 +2,10 @@ package zunza.myshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyShopApplicationTests {
 
 	@Test
