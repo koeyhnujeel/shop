26f43d64d9a054cc49d375be435384a89c8d6207
zunza <fksfks015@naver.com>
2024-04-11 02:57:31 +0900

    Feat: 상품 리뷰 조회 API 구현

--- src/main/java/zunza/myshop/controller/ProductReviewController.java
+++ src/main/java/zunza/myshop/controller/ProductReviewController.java
@@ -1,2 +1,27 @@
-package zunza.myshop.controller;public class ProductReviewController {
+package zunza.myshop.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import zunza.myshop.response.product_detail.ProductReviewResponse;
+import zunza.myshop.service.ProductReviewService;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductReviewController {
+
+	private final ProductReviewService productReviewService;
+
+	@GetMapping("/products/{productId}/reviews")
+	@ResponseStatus(HttpStatus.OK)
+	public List<ProductReviewResponse> ReviewList(@PathVariable("productId") Long productId) {
+
+		return productReviewService.findReviewList(productId);
+	}
 }
--- src/main/java/zunza/myshop/service/ProductReviewService.java
+++ src/main/java/zunza/myshop/service/ProductReviewService.java
@@ -1,2 +1,22 @@
-package zunza.myshop.service;public class ProductReviewService {
+package zunza.myshop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import zunza.myshop.repository.ProductReviewRepository;
+import zunza.myshop.response.product_detail.ProductReviewResponse;
+
+@Service
+@RequiredArgsConstructor
+public class ProductReviewService {
+
+	private final ProductReviewRepository productReviewRepository;
+
+	public List<ProductReviewResponse> findReviewList(Long productId) {
+		return productReviewRepository.findReviewsByProductId(productId).stream()
+			.map(ProductReviewResponse::from)
+			.toList();
+	}
 }
