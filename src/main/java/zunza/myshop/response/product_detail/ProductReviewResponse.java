d947265ec86c7758e770b07792fc7d4161b6269a
zunza <fksfks015@naver.com>
2024-04-11 02:02:14 +0900

    Feat: userId 리뷰 응답 데이터에 추가

--- src/main/java/zunza/myshop/response/product_detail/ProductReviewResponse.java
+++ src/main/java/zunza/myshop/response/product_detail/ProductReviewResponse.java
@@ -9,12 +9,14 @@ import zunza.myshop.domain.ProductReview;
 public class ProductReviewResponse {
 
 	private Long id;
+	private Long userId;
 	private String nickname;
 	private String content;
 	private String createdAt;
 
-	private ProductReviewResponse(Long id, String nickname, String content, String createdAt) {
+	private ProductReviewResponse(Long id, Long userId, String nickname, String content, String createdAt) {
 		this.id = id;
+		this.id = userId;
 		this.nickname = nickname;
 		this.content = content;
 		this.createdAt = createdAt;
@@ -23,6 +25,7 @@ public class ProductReviewResponse {
 	public static ProductReviewResponse from(ProductReview productReview) {
 		return new ProductReviewResponse(
 			productReview.getId(),
+			productReview.getUser().getId(),
 			productReview.getUser().getNickname(),
 			productReview.getContent(),
 			productReview.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
