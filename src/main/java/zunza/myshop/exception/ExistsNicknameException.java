e569e07fffbc1516527b5db231a7772fbf24a33b
zunza <fksfks015@naver.com>
2024-04-12 03:19:13 +0900

    Feat: 닉네임 중복 예외 클래스 추가

--- src/main/java/zunza/myshop/exception/ExistsNicknameException.java
+++ src/main/java/zunza/myshop/exception/ExistsNicknameException.java
@@ -1,2 +1,16 @@
-package zunza.myshop.exception;public class ExistsNicknameException {
+package zunza.myshop.exception;
+
+public class ExistsNicknameException extends CustomException {
+
+	private final static String MESSAGE = "잘못된 요청입니다.";
+
+	public ExistsNicknameException() {
+		super(MESSAGE);
+		addError("nickname", "사용 중인 닉네임입니다.");
+	}
+
+	@Override
+	public int getStatusCode() {
+		return 409;
+	}
 }
