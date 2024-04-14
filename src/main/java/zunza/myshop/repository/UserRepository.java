71ca21a912f329ecce5c8153e1b812d3fd9b391d
zunza <fksfks015@naver.com>
2024-04-14 23:18:34 +0900

    Feat: UserRepository 인터페이스 추가

--- src/main/java/zunza/myshop/repository/UserRepository.java
+++ src/main/java/zunza/myshop/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package zunza.myshop.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Boolean existsByNickname(String nickname);
 
 	Boolean existsByEmail(String email);
+
+	Optional<User> findByEmail(String email);
 }
