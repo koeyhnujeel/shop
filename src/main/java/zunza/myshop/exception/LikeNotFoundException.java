68c1b90b3b0d21287c49038f67f035d7fb149eea
zunza <fksfks015@naver.com>
2024-04-15 20:35:02 +0900

    Feat: LikeNotFoundException 클래스 추가

--- src/main/java/zunza/myshop/exception/LikeNotFoundException.java
+++ src/main/java/zunza/myshop/exception/LikeNotFoundException.java
@@ -1,2 +1,17 @@
-package zunza.myshop.exception;public class LikeNotFoundException {
+package zunza.myshop.exception;
+
+public class LikeNotFoundException extends CustomException {
+
+	private static final String MESSAGE ="잘못된 요청입니다.";
+
+
+	public LikeNotFoundException() {
+		super(MESSAGE);
+		addError("Like", "존재하지 않는 좋아요 데이터입니다.");
+	}
+
+	@Override
+	public int getStatusCode() {
+		return 404;
+	}
 }
