9a20db68ab706a254419ac16a10dac5eab926198
zunza <fksfks015@naver.com>
2024-04-15 23:05:19 +0900

    Feat: PermissionDeniedException 클래스 추가

--- src/main/java/zunza/myshop/exception/PermissionDeniedException.java
+++ src/main/java/zunza/myshop/exception/PermissionDeniedException.java
@@ -1,2 +1,16 @@
-package zunza.myshop.exception;public class PermissionDeniedException {
+package zunza.myshop.exception;
+
+public class PermissionDeniedException extends CustomException {
+
+	private static final String MESSAGE = "잘못된 요청입니다.";
+
+	public PermissionDeniedException() {
+		super(MESSAGE);
+		addError("error", "해당 요청에 대한 권한이 없습니다.");
+	}
+
+	@Override
+	public int getStatusCode() {
+		return 403;
+	}
 }
