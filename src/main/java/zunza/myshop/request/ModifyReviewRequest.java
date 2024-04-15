bc83ba6d7e15df94f86754552a9228426efc8a05
zunza <fksfks015@naver.com>
2024-04-15 23:06:33 +0900

    Feat: 리뷰 수정 요청 클래스 추가

---
+++ src/main/java/zunza/myshop/request/ModifyReviewRequest.java
@@ -0,0 +1,11 @@
+package zunza.myshop.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ModifyReviewRequest {
+
+	private String content;
+}
