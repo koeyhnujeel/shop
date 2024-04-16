23a54c1d76436f6ec15fccb8d4edfaedaa633ff3
zunza <fksfks015@naver.com>
2024-04-16 22:26:32 +0900

    Feat: 사용자 정보 수정 API 구현

---
+++ src/main/java/zunza/myshop/controller/UserController.java
@@ -0,0 +1,39 @@
+package zunza.myshop.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import zunza.myshop.request.AuthRequest;
+import zunza.myshop.request.ModifyProfileRequest;
+import zunza.myshop.service.UserService;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping("/user/profile/auth")
+	@ResponseStatus(HttpStatus.OK)
+	public void auth(
+		@AuthenticationPrincipal Long userId,
+		@RequestBody AuthRequest req) {
+
+		userService.checkPassword(userId, req);
+	}
+
+	@PatchMapping("/user/modify-profile")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void profileModify(
+		@AuthenticationPrincipal Long userId,
+		@RequestBody ModifyProfileRequest req) {
+
+		userService.modifyProfile(userId, req);
+	}
+}
---
+++ src/main/java/zunza/myshop/service/UserService.java
@@ -0,0 +1,38 @@
+package zunza.myshop.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import zunza.myshop.domain.User;
+import zunza.myshop.exception.PermissionDeniedException;
+import zunza.myshop.exception.UserNotFoundException;
+import zunza.myshop.repository.UserRepository;
+import zunza.myshop.request.AuthRequest;
+import zunza.myshop.request.ModifyProfileRequest;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
+
+	public void checkPassword(Long userId, AuthRequest req) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new UserNotFoundException(userId));
+
+		boolean matches = passwordEncoder.matches(req.getPassword(), user.getPassword());
+		if (!matches) {
+			throw new PermissionDeniedException();
+		}
+	}
+	@Transactional
+	public void modifyProfile(Long userId, ModifyProfileRequest req) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new UserNotFoundException(userId));
+
+		user.profileUpdate(req);
+	}
+}
