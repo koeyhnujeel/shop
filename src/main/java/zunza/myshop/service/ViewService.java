02fba6f8e9f7232ce8ead5973c173d1ebdea544b
zunza <fksfks015@naver.com>
2024-04-27 16:27:17 +0900

    Feat: 조회수 캐시 적용 읽기 전략: Look Aside

--- src/main/java/zunza/myshop/service/ViewService.java
+++ src/main/java/zunza/myshop/service/ViewService.java
@@ -1,9 +1,12 @@
 package zunza.myshop.service;
 
+import java.util.Objects;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import zunza.myshop.repository.ViewRepository;
+import zunza.myshop.repository.redis.CustomRedisRepository;
 import zunza.myshop.response.product_detail.ViewResponse;
 
 @Service
@@ -11,9 +14,18 @@ import zunza.myshop.response.product_detail.ViewResponse;
 public class ViewService {
 
 	private final ViewRepository viewRepository;
+	private final CustomRedisRepository redisRepository;
 
 	public ViewResponse count(Long productId) {
+		Object cachedView = redisRepository.get("view::" + productId);
+		if (Objects.nonNull(cachedView)) {
+			return (ViewResponse)cachedView;
+		}
+
 		Long count = viewRepository.countByProductId(productId);
+		redisRepository.set("view::" + productId, count);
+		redisRepository.setTimeToLive("view::" + productId, 60);
+
 		return ViewResponse.from(count);
 	}
 }
